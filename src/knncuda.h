e33e65c9cd1d88b5094f97b3066ccaf6cea1e681
orkuntemiz <58005949+orkuntemiz@users.noreply.github.com>
2021-07-07 16:15:31 +0300

    Update knncuda.h

--- src/knncuda.h
+++ src/knncuda.h
@@ -3,18 +3,18 @@
  * This implementation uses global memory to store reference and query points.
  *
  * @param ref        refence points
- * @param ref_nb     number of reference points
+ * @param ref_num     number of reference points
  * @param query      query points
- * @param query_nb   number of query points
+ * @param query_num   number of query points
  * @param dim        dimension of points
  * @param k          number of neighbors to consider
  * @param knn_dist   output array containing the query_nb x k distances
  * @param knn_index  output array containing the query_nb x k indexes
  */
 bool knn_cuda_global(const float * ref,
-                     int           ref_nb,
+                     int           ref_num,
                      const float * query,
-                     int           query_nb,
+                     int           query_num,
                      int           dim,
                      int           k,
                      float *       knn_dist,
@@ -26,18 +26,18 @@ bool knn_cuda_global(const float * ref,
  * This implementation uses texture memory for storing reference points  and memory to store query points.
  *
  * @param ref        refence points
- * @param ref_nb     number of reference points
+ * @param ref_um     number of reference points
  * @param query      query points
- * @param query_nb   number of query points
+ * @param query_um   number of query points
  * @param dim        dimension of points
  * @param k          number of neighbors to consider
  * @param knn_dist   output array containing the query_nb x k distances
  * @param knn_index  output array containing the query_nb x k indexes
  */
 bool knn_cuda_texture(const float * ref,
-                      int           ref_nb,
+                      int           ref_num,
                       const float * query,
-                      int           query_nb,
+                      int           query_num,
                       int           dim,
                       int           k,
                       float *       knn_dist,
